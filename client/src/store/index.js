712b01d9821c9209e9da412f40945e2aec43939e
Tomasz Chyc <tomek.chyc55@gmail.com>
2020-01-18 15:25:57 +0100

    vuex

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,51 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import { gql } from "apollo-boost";
+import { defaultClient as apolloClient } from "../main.js";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    posts: [],
+    loading: false
+  },
+  getters: {
+    posts: state => state.posts,
+    loading: state => state.loading
   },
   mutations: {
+    setPosts: (state, payload) => {
+      state.posts = payload;
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    }
   },
   actions: {
-  },
-  modules: {
+    getPosts: ({ commit }) => {
+      commit("setLoading", true);
+      apolloClient
+        .query({
+          query: gql`
+            query {
+              getPosts {
+                _id
+                title
+                imageUrl
+              }
+            }
+          `
+        })
+        .then(({ data }) => {
+          commit("setPosts", data.getPosts);
+          commit("setLoading", false);
+        })
+        .catch(err => {
+          commit("setLoading", false);
+          console.log(err);
+        });
+    }
   }
-})
+});
